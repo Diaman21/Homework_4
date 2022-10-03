eba9c1aa9fe810aee92eeb7c0fc099d2c8365bf0
Diaman21 <diaman21@mail.ru>
2022-10-03 16:11:18 +0300

    №25

---
+++ №25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Цикл принимает на вход два числа (А и В) и возводит А в натуральную степень В.
+
+
+Console.Write("Введите число: ");
+double a = Double.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число: ");
+int b = Int32.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine(Power(a, b));
+
+double Power(double x, int y)
+{
+
+    double pow = x;
+    for(int i = 1; i < y; i++)
+    {
+        pow *= x;
+    }
+    return pow;
+}
